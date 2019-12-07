39d83f2a16148db1d0f12b3c5c5ec6c6647e3113
Minecrell <minecrell@minecrell.net>
2019-12-07 12:55:10 +0100

    ARM: dma-mapping: Add support for CONFIG_IOMMU_DMA
    The custom IOMMU DMA implementation in ARM does not really work
    properly. On the other hand, the generic IOMMU DMA can be easily
    enabled and it works just fine.

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -18,6 +18,7 @@
 #include <linux/dma-mapping.h>
 #include <linux/dma-noncoherent.h>
 #include <linux/dma-contiguous.h>
+#include <linux/dma-iommu.h>
 #include <linux/highmem.h>
 #include <linux/memblock.h>
 #include <linux/slab.h>
@@ -2259,7 +2260,7 @@ void arch_setup_dma_ops(struct device *dev, u64 dma_base, u64 size,
 	const struct dma_map_ops *dma_ops;
 
 	dev->archdata.dma_coherent = coherent;
-#ifdef CONFIG_SWIOTLB
+#if defined(CONFIG_SWIOTLB) || defined(CONFIG_IOMMU_DMA)
 	dev->dma_coherent = coherent;
 #endif
 
@@ -2271,12 +2272,17 @@ void arch_setup_dma_ops(struct device *dev, u64 dma_base, u64 size,
 	if (dev->dma_ops)
 		return;
 
-	if (arm_setup_iommu_dma_ops(dev, dma_base, size, iommu))
-		dma_ops = arm_get_iommu_dma_map_ops(coherent);
-	else
-		dma_ops = arm_get_dma_map_ops(coherent);
+	if (iommu)
+		iommu_setup_dma_ops(dev, dma_base, size);
 
-	set_dma_ops(dev, dma_ops);
+	if (!dev->dma_ops) {
+		if (arm_setup_iommu_dma_ops(dev, dma_base, size, iommu))
+			dma_ops = arm_get_iommu_dma_map_ops(coherent);
+		else
+			dma_ops = arm_get_dma_map_ops(coherent);
+
+		set_dma_ops(dev, dma_ops);
+	}
 
 #ifdef CONFIG_XEN
 	if (xen_initial_domain())
@@ -2295,7 +2301,14 @@ void arch_teardown_dma_ops(struct device *dev)
 	set_dma_ops(dev, NULL);
 }
 
-#ifdef CONFIG_SWIOTLB
+#ifdef CONFIG_IOMMU_DMA
+void arch_dma_prep_coherent(struct page *page, size_t size)
+{
+	__dma_clear_buffer(page, size, NORMAL);
+}
+#endif
+
+#if defined(CONFIG_SWIOTLB) || defined(CONFIG_IOMMU_DMA)
 void arch_sync_dma_for_device(phys_addr_t paddr, size_t size,
 		enum dma_data_direction dir)
 {
