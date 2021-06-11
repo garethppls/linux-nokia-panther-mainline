eb2ddaace207eba352b17eb4c00460aabed2cdae
Stephan Gerhold <stephan@gerhold.net>
2021-06-11 10:08:56 +0200

    UPSTREAM: iio: accel: bmc150: Drop misleading/duplicate chip identifiers
    Commit 0ad4bf370176 ("iio:accel:bmc150-accel: Use the chip ID to detect
    sensor variant") stopped using the I2C/ACPI match data to look up the
    bmc150_accel_chip_info. However, the bmc150_accel_chip_info_tbl remained
    as-is, with multiple entries with the same chip_id (e.g. 0xFA for
    BMC150/BMI055/BMA255). This is redundant now because actually the driver
    will always select the first entry with a matching chip_id.
    
    So even if a device probes e.g. with BMA0255 it will end up using the
    chip_info for BMC150. And in general that's fine for now, the entries
    for BMC150/BMI055/BMA255 were exactly the same anyway (except for the
    name, which is replaced with the more accurate one later).
    
    But in this case it's misleading because it suggests that one should
    add even more entries with the same chip_id when adding support for
    new variants. Let's make that more clear by removing the enum with
    the chip identifiers entirely and instead have only one entry per
    chip_id.
    
    Note that we may need to bring back some mechanism to differentiate
    between different chips with the same chip_id in the future.
    For example, BMA250 (currently supported by the bma180 driver) has the
    same chip_id = 0x03 as BMA222 even though they have different channel
    sizes (8 bits vs 10 bits). But in any case, that mechanism would
    need to look quite different from what we have right now.
    
    Cc: Bastien Nocera <hadess@hadess.net>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Reviewed-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Signed-off-by: Stephan Gerhold <stephan@gerhold.net>
    Link: https://lore.kernel.org/r/20210611080903.14384-4-stephan@gerhold.net
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    (cherry picked from commit c06a6aba6835946bcccb9909c98ec110949ea630)

--- drivers/iio/accel/bmc150-accel-core.c
+++ drivers/iio/accel/bmc150-accel-core.c
@@ -1129,8 +1129,8 @@ static const struct iio_chan_spec bma280_accel_channels[] =
 	BMC150_ACCEL_CHANNELS(14);
 
 static const struct bmc150_accel_chip_info bmc150_accel_chip_info_tbl[] = {
-	[bmc150] = {
-		.name = "BMC150A",
+	{
+		.name = "BMA255/BMC150/BMI055",
 		.chip_id = 0xFA,
 		.channels = bmc150_accel_channels,
 		.num_channels = ARRAY_SIZE(bmc150_accel_channels),
@@ -1139,27 +1139,7 @@ static const struct bmc150_accel_chip_info bmc150_accel_chip_info_tbl[] = {
 				 {38344, BMC150_ACCEL_DEF_RANGE_8G},
 				 {76590, BMC150_ACCEL_DEF_RANGE_16G} },
 	},
-	[bmi055] = {
-		.name = "BMI055A",
-		.chip_id = 0xFA,
-		.channels = bmc150_accel_channels,
-		.num_channels = ARRAY_SIZE(bmc150_accel_channels),
-		.scale_table = { {9610, BMC150_ACCEL_DEF_RANGE_2G},
-				 {19122, BMC150_ACCEL_DEF_RANGE_4G},
-				 {38344, BMC150_ACCEL_DEF_RANGE_8G},
-				 {76590, BMC150_ACCEL_DEF_RANGE_16G} },
-	},
-	[bma255] = {
-		.name = "BMA0255",
-		.chip_id = 0xFA,
-		.channels = bmc150_accel_channels,
-		.num_channels = ARRAY_SIZE(bmc150_accel_channels),
-		.scale_table = { {9610, BMC150_ACCEL_DEF_RANGE_2G},
-				 {19122, BMC150_ACCEL_DEF_RANGE_4G},
-				 {38344, BMC150_ACCEL_DEF_RANGE_8G},
-				 {76590, BMC150_ACCEL_DEF_RANGE_16G} },
-	},
-	[bma250e] = {
+	{
 		.name = "BMA250E",
 		.chip_id = 0xF9,
 		.channels = bma250e_accel_channels,
@@ -1169,7 +1149,7 @@ static const struct bmc150_accel_chip_info bmc150_accel_chip_info_tbl[] = {
 				 {153277, BMC150_ACCEL_DEF_RANGE_8G},
 				 {306457, BMC150_ACCEL_DEF_RANGE_16G} },
 	},
-	[bma222] = {
+	{
 		.name = "BMA222",
 		.chip_id = 0x03,
 		.channels = bma222e_accel_channels,
@@ -1183,7 +1163,7 @@ static const struct bmc150_accel_chip_info bmc150_accel_chip_info_tbl[] = {
 				 {625000, BMC150_ACCEL_DEF_RANGE_8G},
 				 {1250000, BMC150_ACCEL_DEF_RANGE_16G} },
 	},
-	[bma222e] = {
+	{
 		.name = "BMA222E",
 		.chip_id = 0xF8,
 		.channels = bma222e_accel_channels,
@@ -1193,8 +1173,8 @@ static const struct bmc150_accel_chip_info bmc150_accel_chip_info_tbl[] = {
 				 {612915, BMC150_ACCEL_DEF_RANGE_8G},
 				 {1225831, BMC150_ACCEL_DEF_RANGE_16G} },
 	},
-	[bma280] = {
-		.name = "BMA0280",
+	{
+		.name = "BMA280",
 		.chip_id = 0xFB,
 		.channels = bma280_accel_channels,
 		.num_channels = ARRAY_SIZE(bma280_accel_channels),
--- drivers/iio/accel/bmc150-accel.h
+++ drivers/iio/accel/bmc150-accel.h
@@ -4,16 +4,6 @@
 
 struct regmap;
 
-enum {
-	bmc150,
-	bmi055,
-	bma255,
-	bma250e,
-	bma222,
-	bma222e,
-	bma280,
-};
-
 int bmc150_accel_core_probe(struct device *dev, struct regmap *regmap, int irq,
 			    const char *name, bool block_supported);
 int bmc150_accel_core_remove(struct device *dev);
