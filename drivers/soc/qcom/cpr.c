8af670e21bef1e17ae0faa55b7d52192cc58ee69
Lanik123 <daniilt971@gmail.com>
2021-08-03 20:35:59 +0300

    power: avs: qcom-cpr: Add support for msm8917, msm8937 and msm8940

--- drivers/soc/qcom/cpr.c
+++ drivers/soc/qcom/cpr.c
@@ -1344,6 +1344,264 @@ static int cpr_find_initial_corner(struct cpr_drv *drv)
 	return 0;
 }
 
+static const struct cpr_desc msm8917_cpr_desc = {
+	.num_fuse_corners = 3,
+	.min_diff_quot = CPR_FUSE_MIN_QUOT_DIFF,
+	.step_quot = (int []){ 16, 16, 16, },
+	.timer_delay_us = 5000,
+	.timer_cons_up = 0,
+	.timer_cons_down = 2,
+	.up_threshold = 2,
+	.down_threshold = 4,
+	.idle_clocks = 15,
+	.gcnt_us = 1,
+	.vdd_apc_step_up_limit = 1,
+	.vdd_apc_step_down_limit = 1,
+	.cpr_fuses = {
+		.init_voltage_step = 10000,
+		.init_voltage_width = 6,
+		.fuse_corner_data = (struct fuse_corner_data[]){
+			/* fuse corner 0 */
+			{
+				.ref_uV = 1155000,
+				.max_uV = 1155000,
+				.min_uV = 1050000,
+				.max_volt_scale = 0,
+				.max_quot_scale = 0,
+				.quot_offset = 0,
+				.quot_scale = 1,
+				.quot_adjust = 0,
+				.quot_offset_scale = 5,
+				.quot_offset_adjust = 0,
+			},
+			/* fuse corner 1 */
+			{
+				.ref_uV = 1225000,
+				.max_uV = 1225000,
+				.min_uV = 1050000,
+				.max_volt_scale = 2000,
+				.max_quot_scale = 1400,
+				.quot_offset = 0,
+				.quot_scale = 1,
+				.quot_adjust = 0,
+				.quot_offset_scale = 5,
+				.quot_offset_adjust = 0,
+			},
+			/* fuse corner 2 */
+			{
+				.ref_uV = 1350000,
+				.max_uV = 1350000,
+				.min_uV = 1090000,
+				.max_volt_scale = 2000,
+				.max_quot_scale = 1400,
+				.quot_offset = 0,
+				.quot_scale = 1,
+				.quot_adjust = 50,
+				.quot_offset_scale = 5,
+				.quot_offset_adjust = 0,
+			},
+		},
+	},
+};
+
+static const struct acc_desc msm8917_acc_desc = {
+	.settings = (struct reg_sequence[]){
+		{ 0xb120, 0x30c30c3 },
+		{ 0xb124, 0x30c3 },
+		
+		{ 0xb120, 0x1041041 },
+		{ 0xb124, 0x1041 },
+
+		{ 0xb120, 0x0 },
+		{ 0xb124, 0x0 },
+	},
+	.config = (struct reg_sequence[]){
+		{ 0xb138, 0xff },
+		{ 0xb130, 0x5555 },
+	},
+	.num_regs_per_fuse = 2,
+};
+
+static const struct cpr_acc_desc msm8917_cpr_acc_desc = {
+	.cpr_desc = &msm8917_cpr_desc,
+	.acc_desc = &msm8917_acc_desc,
+};
+
+static const struct cpr_desc msm8937_cpr_desc = {
+	.num_fuse_corners = 3,
+	.min_diff_quot = CPR_FUSE_MIN_QUOT_DIFF,
+	.step_quot = (int []){ 10, 10, 10, },
+	.timer_delay_us = 5000,
+	.timer_cons_up = 0,
+	.timer_cons_down = 2,
+	.up_threshold = 2,
+	.down_threshold = 4,
+	.idle_clocks = 15,
+	.gcnt_us = 1,
+	.vdd_apc_step_up_limit = 1,
+	.vdd_apc_step_down_limit = 1,
+	.cpr_fuses = {
+		.init_voltage_step = 10000,
+		.init_voltage_width = 6,
+		.fuse_corner_data = (struct fuse_corner_data[]){
+			/* fuse corner 0 */
+			{
+				.ref_uV = 1155000,
+				.max_uV = 1155000,
+				.min_uV = 1050000,
+				.max_volt_scale = 0,
+				.max_quot_scale = 0,
+				.quot_offset = 0,
+				.quot_scale = 1,
+				.quot_adjust = -20,
+				.quot_offset_scale = 5,
+				.quot_offset_adjust = 0,
+			},
+			/* fuse corner 1 */
+			{
+				.ref_uV = 1225000,
+				.max_uV = 1225000,
+				.min_uV = 1050000,
+				.max_volt_scale = 2000,
+				.max_quot_scale = 1400,
+				.quot_offset = 0,
+				.quot_scale = 1,
+				.quot_adjust = -40,
+				.quot_offset_scale = 5,
+				.quot_offset_adjust = 0,
+			},
+			/* fuse corner 2 */
+			{
+				.ref_uV = 1350000,
+				.max_uV = 1350000,
+				.min_uV = 1090000,
+				.max_volt_scale = 2000,
+				.max_quot_scale = 1400,
+				.quot_offset = 0,
+				.quot_scale = 1,
+				.quot_adjust = -20,
+				.quot_offset_scale = 5,
+				.quot_offset_adjust = 0,
+			},
+		},
+	},
+};
+
+static const struct acc_desc msm8937_acc_desc = {
+	.settings = (struct reg_sequence[]){
+		{ 0xb120, 0x30c30c3 },
+		{ 0xb124, 0x30c3 },
+		{ 0xf000, 0x6060606 },
+
+		{ 0xb120, 0x1041041 },
+		{ 0xb124, 0x1041 },
+		{ 0xf000, 0x2020202 },
+
+		{ 0xb120, 0x0 },
+		{ 0xb124, 0x0 },
+		{ 0xf000, 0x0 },
+	},
+	.config = (struct reg_sequence[]){
+		{ 0xb138, 0xff },
+		{ 0xb130, 0x5555 },
+		{ 0xf004, 0x55 },
+	},
+	.num_regs_per_fuse = 3,
+};
+
+static const struct cpr_acc_desc msm8937_cpr_acc_desc = {
+	.cpr_desc = &msm8937_cpr_desc,
+	.acc_desc = &msm8937_acc_desc,
+};
+
+static const struct cpr_desc msm8940_cpr_desc = {
+	.num_fuse_corners = 3,
+	.min_diff_quot = CPR_FUSE_MIN_QUOT_DIFF,
+	.step_quot = (int []){ 10, 10, 10, },
+	.timer_delay_us = 5000,
+	.timer_cons_up = 0,
+	.timer_cons_down = 2,
+	.up_threshold = 2,
+	.down_threshold = 4,
+	.idle_clocks = 15,
+	.gcnt_us = 1,
+	.vdd_apc_step_up_limit = 1,
+	.vdd_apc_step_down_limit = 1,
+	.cpr_fuses = {
+		.init_voltage_step = 10000,
+		.init_voltage_width = 6,
+		.fuse_corner_data = (struct fuse_corner_data[]){
+			/* fuse corner 0 */
+			{
+				.ref_uV = 1155000,
+				.max_uV = 1155000,
+				.min_uV = 1050000,
+				.max_volt_scale = 0,
+				.max_quot_scale = 0,
+				.quot_offset = 0,
+				.quot_scale = 1,
+				.quot_adjust = 0,
+				.quot_offset_scale = 5,
+				.quot_offset_adjust = 0,
+			},
+			/* fuse corner 1 */
+			{
+				.ref_uV = 1225000,
+				.max_uV = 1225000,
+				.min_uV = 1050000,
+				.max_volt_scale = 2000,
+				.max_quot_scale = 1400,
+				.quot_offset = 0,
+				.quot_scale = 1,
+				.quot_adjust = 0,
+				.quot_offset_scale = 5,
+				.quot_offset_adjust = 0,
+			},
+			/* fuse corner 2 */
+			{
+				.ref_uV = 1350000,
+				.max_uV = 1350000,
+				.min_uV = 1090000,
+				.max_volt_scale = 2000,
+				.max_quot_scale = 1400,
+				.quot_offset = 0,
+				.quot_scale = 1,
+				.quot_adjust = 0,
+				.quot_offset_scale = 5,
+				.quot_offset_adjust = 0,
+			},
+		},
+	},
+};
+
+static const struct acc_desc msm8940_acc_desc = {
+	.settings = (struct reg_sequence[]){
+		{ 0xb130, 0x555555 },
+		{ 0xb120, 0x30C30C3 },
+		{ 0xb124, 0x30C30C3 },
+		{ 0xb128, 0x00000C3 },
+
+		{ 0xb130, 0x555555 },
+		{ 0xb120, 0x1041041 },
+		{ 0xb124, 0x1041041 },
+		{ 0xb128, 0x0000041 },
+
+		{ 0xb130, 0x555555 },
+		{ 0xb120, 0x0 },
+		{ 0xb124, 0x0 },
+		{ 0xb128, 0x0 },
+	},
+	.config = (struct reg_sequence[]){
+		{ 0xb138, 0xfff },
+	},
+	.num_regs_per_fuse = 4,
+};
+
+static const struct cpr_acc_desc msm8940_cpr_acc_desc = {
+	.cpr_desc = &msm8940_cpr_desc,
+	.acc_desc = &msm8940_acc_desc,
+};
+
 static const struct cpr_desc qcs404_cpr_desc = {
 	.num_fuse_corners = 3,
 	.min_diff_quot = CPR_FUSE_MIN_QUOT_DIFF,
@@ -1735,6 +1993,9 @@ static int cpr_remove(struct platform_device *pdev)
 
 static const struct of_device_id cpr_match_table[] = {
 	{ .compatible = "qcom,qcs404-cpr", .data = &qcs404_cpr_acc_desc },
+	{ .compatible = "qcom,msm8917-cpr", .data = &msm8917_cpr_acc_desc },
+	{ .compatible = "qcom,msm8937-cpr", .data = &msm8937_cpr_acc_desc },
+	{ .compatible = "qcom,msm8940-cpr", .data = &msm8940_cpr_acc_desc },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, cpr_match_table);
